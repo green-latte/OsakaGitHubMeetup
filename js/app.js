4022fb3ad057a9aac9c67954dce8285f4a22b000
Kousuke Takeuchi <k.takeuchi@warrantee.co.jp>
2015-12-15 02:44:03 +0900

    とりあえずスライドを追加

--- js/app.js
+++ js/app.js
@@ -6,24 +6,22 @@ location.search.replace( /[A-Z0-9]+?=(\w*)/gi, function(a) {
 
 // Reveal を初期化
 Reveal.initialize({
-    // 画面右下にある十字キーみたいなやつの表示/非表示(false)
-    controls: (query.controls) ? (query.controls=="true") : true,
+  controls: true,
+  progress: true,
+  history: true,
+  center: true,
 
-    // 画面下にあるページ進行状態を表すバーの表示/非表示(false)
-    progress: (query.progress) ? (query.progress=="true") : true,
+  theme: query.theme || "simple",    // default/neon
+  transition: Reveal.getQueryHash().transition || 'fade', // default/cube/page/concave/zoom/linear/fade/none
 
-    // ハッシュタグ(url の #~)でページ位置を記憶するかどうかのフラグ(false)
-    history: (query.history) ? (query.history=="true") : true,
-
-    // transition の変化の感じ
-    transition: query.transition || "fade",  // default/concave/linear/cube/page
-
-    // テーマ切り替え
-    theme: query.theme || "serif",    // default/neon
-
-    // リンク(aタグ)にマウスを乗っけた時に 3D 演出をするかどうかのフラグ(true)
-    rollingLinks: (query.rollingLinks) ? (query.rollingLinks=="true") : true,
+  dependencies: [
+    { src: '/vendor/lib/js/markdown/marked.js', condition: function() { return !document.body.classList; } },
+    { src: '/vendor/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+    { src: '/vendor/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+    { src: '/vendor/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+    { src: '/vendor/plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
+    { src: '/vendor/plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } }
+    // { src: 'plugin/search/search.js', async: true, condition: function() { return !!document.body.classList; } }
+    // { src: 'plugin/remotes/remotes.js', async: true, condition: function() { return !!document.body.classList; } }
+  ]
 });
-
-// コードハイライト
-hljs.initHighlightingOnLoad();
